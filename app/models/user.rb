27759fa529d6056be66ee2083819a4b6fd31ed36
Lenard Gutierrez <lenguti@gmail.com>
2015-06-05 13:34:33 -0400

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    user = find_or_create_by(uid: auth["uid"], provider: auth["provider"])
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.name = auth.info.name
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user.save!
+    user
+  end
+end
